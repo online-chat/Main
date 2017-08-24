22fc88db9df197957869871cb0994be8aadec452
online-chat <31315759+online-chat@users.noreply.github.com>
2017-08-24 22:48:59 +0800

    Add files via upload.
    Main files.

---
+++ Countdown.js
@@ -0,0 +1,23 @@
+var seconds = 14;
+var foo;
+
+function redirect() {
+  window.open('https://c.tian.science:4545', '_self');
+}
+
+function updateSecs() {
+    document.getElementById("seconds").innerHTML = seconds;
+    seconds--;
+    if (seconds == -1) {
+        clearInterval(foo);
+        redirect();
+    }
+}
+
+function countdownTimer() {
+    foo = setInterval(function () {
+        updateSecs()
+    }, 966);
+}
+
+countdownTimer();
